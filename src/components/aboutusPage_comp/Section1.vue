353ca9d95d90d9bd55619a7263b3991493111507
Nann Koungkea <nannkoungkea00@gmail.com>
2025-04-12 22:31:39 +0700

    Add About Us Page Sections with Team and Story Details
    - Created Section2.vue for the "Our Story" section, detailing the origin of EduFunApp.
    - Introduced Section3-comp.vue for individual team member profiles with props for name, role, and image.
    - Developed Section3.vue to display team members and advisors, utilizing Section3-comp for rendering.

--- src/components/aboutusPage_comp/Section1.vue
+++ src/components/aboutusPage_comp/Section1.vue
@@ -1,12 +1,31 @@
 <template>
-    <div class="w-full h-[200px] lg:pt-30 pt-15 lg:pb-30 pb-15 bg-red-200">
+    <div class="w-full flex lg:flex-row flex-col h-fit lg:pt-30 pt-5 lg:pb-5 pb-1 lg:pl-0 lg:pr-0 pl-5 pr-5">
         <!-- left side -->
-        <div class="w-[50%]">
+        <div class="lg:w-[50%] w-full h-full">
+            <div class="w-full h-full flex flex-col justify-center items-center">
+                <div class="flex flex-col justify-center items-start gap-3 pt-10">
+                    <h5 class="text-black font-bold ">About Us</h5>
+                    <h2 class="text-black font-bold max-w-[577px]">
+                        Our goal is to 
+                        <span class="font-bold primary-gradient-text"
+                        >make learning FUN and INTERACTIVE </span>
+                        to EVERYBODY AND EVERYWHERE
+                    </h2>
+                    <p class="text-black max-w-[500px]">
+                        DICE is a super web application where students and lecturers can
+                        manage and connect with each other.
+                    </p>
+                </div>
+                
+            </div>
+
             
         </div>
         <!-- right side -->
-        <div class="w-[50%]">
-
+        <div class="w-full lg:w-[50%]">
+            <div class="flex justify-center items-center">
+                <img src="/landingPage_img/aboutus_Sec1.png" alt="" class="w-full h-auto max-w-[552px] max-h-[552px]">
+            </div>
         </div>
 
     </div>
---
+++ src/components/aboutusPage_comp/Section3-comp.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="flex flex-col lg:gap-3 gap-1 lg:max-w-[291px] w-full justify-center items-center">
+        <img :src="imageSrc" alt="" class="w-full h-auto max-w-[552px] max-h-[552px]">
+        <h4 class="font-bold text-black">{{name}}</h4>
+        <h6 class="text-black">{{role}}</h6>
+    </div>
+
+</template>
+<script setup>
+
+defineProps({
+    name:{
+        type: String,
+        default: "Nann Koungkea"
+    },
+    role:{
+        type: String,
+        default: "Front-End Dev"
+    },
+    imageSrc: {
+        type: String,
+        default: "/landingPage_img/sec3-avatar.png"
+    }
+})
+</script>
