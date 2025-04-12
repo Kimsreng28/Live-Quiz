32af2791f502c0adf5c3a4b1fc5b4fd4293606a6
Nann Koungkea <nannkoungkea00@gmail.com>
2025-04-12 19:40:13 +0700

    config router, create landingpage_layout

--- src/pages/LandingPage_layout.vue
+++ src/pages/LandingPage_layout.vue
@@ -1,10 +1,5 @@
 <script setup>
-import Button from "primevue/button";
 import Header from "../components/landingPage_comp/Header.vue";
-import Section1 from "../components/landingPage_comp/Section1.vue";
-import Section2 from "../components/landingPage_comp/Section2.vue";
-import Section3 from "../components/landingPage_comp/Section3.vue";
-import Section4 from "../components/landingPage_comp/Section4.vue";
 import Footer from "../components/landingPage_comp/Footer.vue";
 import { RouterView } from 'vue-router';
 
